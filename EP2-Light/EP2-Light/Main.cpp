cf7ca159c8c2631e55c21cfc81217b0ae4193384
slightdream <492389329@qq.com>
2025-11-28 11:31:35 +0800

    lighting maps

--- EP2-Light/EP2-Light/Main.cpp
+++ EP2-Light/EP2-Light/Main.cpp
@@ -1,6 +1,7 @@
 //#include "1_color/Color.h"
 //#include "2_basiclighting/BasicLighting.h"
-#include "3_material/Material.h"
+//#include "3_material/Material.h"
+#include "4_lightingmaps/LightingMaps.h"
 
 int main() {
 	Show();
